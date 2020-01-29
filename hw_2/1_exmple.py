4a1547c5345c9fc81ab09a57a26ca968f984486a
Targenas <60173876+Targenas@users.noreply.github.com>
2020-01-29 12:18:50 +0300

    Add files via upload

---
+++ hw_2/1_exmple.py
@@ -0,0 +1,18 @@
+import turtle
+
+def example():
+    import turtle
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+
+    pass
+
+if __name__ == "__main__":
+    example()
---
+++ hw_2/2_rectangle.py
@@ -0,0 +1,14 @@
+import turtle
+def rectangle():
+    turtle.width(6)
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+
+    pass
+if __name__ == "__main__":
+    rectangle()
